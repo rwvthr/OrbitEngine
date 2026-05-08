355e9a021c81d953e41f5f74f06889749c49d280
rwvthr <mrls9@aluno.ifal.edu.br>
2026-05-08 10:56:43 -0300

    updt

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 with open('aboutproject.txt', 'r', encoding='utf-8') as arquivo:
     conteudo = arquivo.read()
     print(conteudo)
+
+with open('manual.txt', 'r', encoding='utf-8') as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
+
+with open('notices.txt', 'r', encoding='utf-8') as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
