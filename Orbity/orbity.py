075a61673ea1e3ec0ad4ffed46c2aade911dcdee
rwvthr <mrls9@aluno.ifal.edu.br>
2026-05-09 14:54:45 -0300

    dsdsdsdsd

--- Orbity/orbity.py
+++ Orbity/orbity.py
@@ -3,6 +3,7 @@ from os import system
 import importlib
 import sys
 
+#Função limpa terminal
 def clear_screen():
     from os import system
     import platform
@@ -11,6 +12,7 @@ def clear_screen():
     else:
         system('clear')
 
+#Função animação de carregamento 
 def loadinganm():
     AZUL = '\033[94m'                      
     ROSA_VIBRANTE = '\033[38;2;255;20;147m' 
@@ -27,6 +29,7 @@ def loadinganm():
             sys.stdout.write(f"\r{AZUL}{msg}{RESET} {NEGRITO}{ROSA_VIBRANTE}{animation[i % len(animation)]}{RESET}")
             sys.stdout.flush()
 
+#Função da art em ascii
 def ascii():
     asciiart = r"""     
    _        ___       _     _ _                        
@@ -36,19 +39,14 @@ def ascii():
 ///|\\\    \___/|_|  |_.__/|_|\__|\__, |             
                                   |___/   
 
-         O R B I T Y - v1.0.1        
+         O R B I T Y - v1.0.0.1
+          Author: M.R.L Silva        
 """ 
     return asciiart
 
+#função do menu
 def initmenu():
-    asciiart= ascii()
-
-    header = r"""
-Author: M.R.L Silva;
-Project: Orbity - A rockertics simulation software for educational purposes;
-Institution: Instituto Federal de Educação, Ciência e Tecnologia de Alagoas - IFAL-PIN;
-Advisor: Prof. Dr. Karciano José Silva Santos;
-"""
+    asciiart = ascii()
 
     options1 = r"""
 [01] - More about the project
@@ -59,19 +57,41 @@ Advisor: Prof. Dr. Karciano José Silva Santos;
 """
 
     print(f"\033[38;5;208m{asciiart}\033[0m")
-    print(f"\033[38;2;64;224;208m{header}\033[0m")
     print(f"\033[38;2;64;224;208m{options1}\033[0m")
 
-    pergunta = input('\033[38;2;64;224;208mPlease select an option: \033[0m').strip()
-
-    print(" ")
-
+#Call
 initmenu()
 
+#Pergunta
+pergunta = input('\033[38;2;64;224;208mPlease select an option: \033[0m').strip()
+print(' ')
+
+#Call
 loadinganm()
 clear_screen()
 
-if 
+if pergunta == 1:
+    with open('aboutproject.txt', 'r', encoding='utf-8') as arquivo:
+        conteudo = arquivo.read()
+        print(conteudo)
+
+elif pergunta == 2:
+    with open('notices.txt', 'r', encoding='utf-8') as arquivo:
+        conteudo = arquivo.read()
+        print(conteudo)
+
+elif pergunta == 3:
+    with open('manual.txt', 'r', encoding='utf-8') as arquivo:
+        conteudo = arquivo.read()
+        print(conteudo)
+
+
+
+
+
+
+
+
 
 
 
